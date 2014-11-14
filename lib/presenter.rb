d25d9fffff28e106bde258847ee5a2f91d901ef9
Vishnu Gopal <g.vishnu@gmail.com>
2014-11-14 18:54:48 +0530

    Extracting out presenter & making a binary.

--- lib/presenter.rb
+++ lib/presenter.rb
@@ -1,22 +1,42 @@
 require_relative "grid"
 
 class Presenter
+  COORDINATE_MAX_X = 20
+  COORDINATE_MAX_Y = 20
+  
   attr_accessor :grid
+  
+  # Make a presenter with an example grid
+  def self.with_example_grid
+    grid = Grid.new
+
+    (0..COORDINATE_MAX_X).each do |x|
+      (0..COORDINATE_MAX_Y).each do |y|
+        c = Cell.new
+        coordinate = Coordinate.new(x, y)
+        c.coordinate = coordinate
+        c.make_alive if x % 3 == 0
+        grid.cells << c
+      end
+    end
+    
+    new(grid)
+  end 
   
   def initialize(grid)
     @grid = grid
   end
   
-  def iterate
+  def evolve
     self.grid = self.grid.evolve
   end
   
   def show
-    (0..10).each do |x|
-      (0..10).each do |y|
-        cell = self.grid.cell_at(x, y)
+    (0..COORDINATE_MAX_X).each do |x|
+      (0..COORDINATE_MAX_Y).each do |y|
+        cell = self.grid.cell_at(Coordinate.new(x, y))
         if cell && cell.alive?
-          print "O"
+          print "X"
         else
           print " "
         end
@@ -24,24 +44,13 @@ class Presenter
       print "\n"
     end
   end
-end
-
-g = Grid.new
-
-(0..10).each do |x|
-  (0..10).each do |y|
-    c = Cell.new
-    coordinate = Coordinate.new(x, y)
-    c.coordinate = coordinate
-    c.make_alive if x % 2 == 0
-    g.cells << c
+  
+  def play
+    loop do
+      show
+      evolve
+      sleep(0.5)
+    end
   end
 end
 
-p = Presenter.new(g)
-
-loop do
-  p.show
-  p.iterate
-  sleep(0.5)
-end
